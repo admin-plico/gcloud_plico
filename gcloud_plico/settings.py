568d843d6827e0efabae63836c224d5cae94f63a
David Desktop <dlaquerre@gmail.com>
2025-07-08 16:24:41 +1000

    updates2

--- gcloud_plico/settings.py
+++ gcloud_plico/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = str(BASE_DIR / 'static')
+STATIC_URL = str(BASE_DIR / 'static/')
 STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
