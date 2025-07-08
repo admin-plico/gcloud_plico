635e08309a4f9f937a82d5346ef5bad35f211fec
David Desktop <dlaquerre@gmail.com>
2025-07-08 15:46:38 +1000

    first commit of the project

---
+++ gcloud_plico/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for gcloud_plico project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gcloud_plico.settings')
+
+application = get_wsgi_application()
